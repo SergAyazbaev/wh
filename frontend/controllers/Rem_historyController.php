859fa8931d3b6e613cc9cd8f6e5b502b540dfdee
SergAyazbaev <sergayazbaev@gmail.com>
2022-07-12 18:03:33 +0600

    2

--- frontend/controllers/Rem_historyController.php
+++ frontend/controllers/Rem_historyController.php
@@ -408,6 +408,7 @@ class Rem_historyController extends Controller
 
 
 
+
           /// Если это АДМИН или Саша
           if( Yii::$app->user->identity->id == 10000 ||
               Yii::$app->user->identity->id == 10012  ){
