c2e3dcaf62947357c2fdb1efd1cfaf6ef185650e
SergAyazbaev <sergayazbaev@gmail.com>
2022-07-15 16:29:14 +0600

    123

--- frontend/controllers/Rem_historyController.php
+++ frontend/controllers/Rem_historyController.php
@@ -820,8 +820,9 @@ class Rem_historyController extends Controller
         //        $decision_all = Rem_history::countDecision_all('now');  // 100%  - 36
 
         $uniq_arr = Rem_history::ArrayUniq_decision('now');
-      
 
+    dd($uniq_arr);
+    
         ///Возвращает массив с БОЛЕЕ подробными неисправностями
         $uniq_arr = Rem_history::ArrayTranslator($uniq_arr);
         ddd($uniq_arr);
